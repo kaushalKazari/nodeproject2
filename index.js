9d7ca3d43e344bae1e893e48dabb28398dfd95ff
Kaushal Kishor <144304191+kaushalKazari@users.noreply.github.com>
2024-02-27 12:54:29 +0530

    v3

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ const app = express();
 app.use(bodyParser.json())
 app.use(cors('http://localhost:4200/')) // it is front end link
 
-app.listen(9001, () =>{console.log('Server started at port 9001')})
+app.listen(3000, () =>{console.log('Server started at port 3000')})
 
 app.use('/employee',router)
